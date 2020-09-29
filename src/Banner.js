12658f96b19cd41532a35002cf71d98a7c5342c8
Anis Mansour <anis@Aniss-MacBook-Pro.local>
2020-09-29 12:22:12 -0700

    added calendar

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import { Button } from '@material-ui/core';
+import React, { useState } from 'react';
 import './Css/Banner.css';
+import DatePicker from './DatePicker';
 
 function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <div className="banner">
-      <h1>banner</h1>
+      <div className="banner_search">
+        {showSearch && <DatePicker />}
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          className="banner_searchButton"
+          variant="outlined"
+        >
+          search dates
+        </Button>
+      </div>
+      <div className="banner_info">
+        <h1>Go Near</h1>
+        <h5>
+          Settle in somewhere new. Discover stays to live, work, or just relax.
+        </h5>
+        <Button variant="outlined"> Explore</Button>
+      </div>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Css/Home.css';
 import Banner from './Banner';
+
 function Home() {
   return (
     <div className="home">
