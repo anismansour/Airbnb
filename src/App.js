cd17f23946c785f88aebc170a92ad1ca957b71aa
Anis Mansour <anis@Aniss-MacBook-Pro.local>
2020-09-30 11:18:37 -0700

    footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Css/App.css';
 import Home from './Home.js';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
